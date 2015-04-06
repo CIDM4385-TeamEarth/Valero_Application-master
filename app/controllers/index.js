8b754cf99a8fd5c651c3aa4315565c22aba4a4e7
Zeka7 <traviswarzecha@gmail.com>
2015-04-06 14:47:44 -0500

    updated controller index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,4 +3,14 @@ function showLocations(e){
 	var showLocations = Alloy.createController("showLocations").getView();
 	showLocations.open();
 }
+
+function storeLocations(e){
+	var storeLocations = Alloy.createController("storeLocations").getView();
+	storeLocations.open();
+}
+
+function tripPlanner(e){
+	var tripPlanner = Alloy.createController("tripPlanner").getView();
+	tripPlanner.open();
+}
 $.index.open();
